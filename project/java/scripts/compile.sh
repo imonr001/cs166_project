6be0414e2f4e674faf0eb8ebe1a9e0c2e468de92
Ali Shaikh <ashai014@wch129-34.cs.ucr.edu>
2022-11-26 19:20:04 -0800

    changed file content to work on virtual lab machine

--- project/java/scripts/compile.sh
+++ project/java/scripts/compile.sh
@@ -3,12 +3,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-19.jdk/Contents/Home
 export PATH=$JAVA_HOME/bin:$PATH
-export CLASSPATH=$CLASSPATH:/Users/ivan/Downloads/postgresql-42.5.0.jar
 
 # compile the java program
 javac -d $DIR/../classes $DIR/../src/Retail.java
 
 #run the java program
 #Use your database name, port number and login
-java -cp $DIR/../classes:$DIR/Users/ivan/Downloads/postgresql-42.5.0.jar Retail "ivan_db" "5432" "ivan"
+java -cp $DIR/../classes:$DIR/../lib/pg73jdbc3.jar Retail $USER"_DB" $PGPORT $USER
 
